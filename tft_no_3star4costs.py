e54e0a6e6e2332ac89c0337029655b1217364773
Michael Cheung Ka Ming <59136711+michaelcheungkm@users.noreply.github.com>
2024-04-28 19:11:21 -0700

    Update tft_no_3star4costs.py

--- tft_no_3star4costs.py
+++ tft_no_3star4costs.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
 
-api_key = 'RGAPI-199c2cc1-385f-49b0-977f-7c84adcbeffe'
+api_key = ''
 watcher = TftWatcher(api_key)
 my_region = 'na1'
 summoner_name = 'Mikulll#NA1'
